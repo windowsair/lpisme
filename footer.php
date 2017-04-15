bf43f4d3896c18f7acc0ced64ef580ca9f90815d
windowsair <3034970641@qq.com>
2017-04-15 21:48:26 +0800

    细节调整

--- footer.php
+++ footer.php
@@ -44,12 +44,6 @@
 	<script src="//cdn.bootcss.com/fastclick/1.0.6/fastclick.min.js" data-no-instant></script>
 	<script src="//cdn.bootcss.com/instantclick/3.0.1/instantclick.min.js" data-no-instant></script>
     <?php if($this->is('index')): ?>
-    <script> var lists = document.getElementsByTagName("link");
-    for (var i = 0; i < lists.length; i++) {
-    if (lists[i].getAttribute("href").indexOf("passage.css") != -1) {
-    lists[i].setAttribute("href","<?php $this->options->themeUrl('style.css'); ?>");
-}
-    </script>
 	<?php else: ?>
 	
 	<script src="<?php $this->options->themeUrl('js/prism.js'); ?>" ></script>
